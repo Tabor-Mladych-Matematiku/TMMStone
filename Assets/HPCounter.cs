3ab59d9c3f49c01155fc62d31ab9b268904ea9a6
malyjir <zemekoulak@gmail.com>
2025-02-24 13:33:16 +0100

    Adding HPCounter

---
+++ Assets/HPCounter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class HPCounter : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer[] circles;
+    [SerializeField] TextMeshProUGUI HPLabel;
+
+    int h;
+    public int Health
+    {
+        get => h;
+        set
+        {
+            h = value;
+            HPLabel.text = value.ToString();
+            circles[0].gameObject.SetActive(h >= 10);
+            circles[1].gameObject.SetActive(h >= 20);
+            circles[2].gameObject.SetActive(h >= 30);
+        }
+    }
+}
