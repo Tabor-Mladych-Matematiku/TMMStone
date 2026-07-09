9c2131d4e6a13db4c518fe541fb877fe1b6ee496
malyjir <zemekoulak@gmail.com>
2026-07-09 14:08:14 +0200

    Adding TODO and bandaid fix for an error caused by improper handling of OnDeath effects

--- Assets/Resources/CardData/Scripts/GULAG/Bitka o příděly.cs
+++ Assets/Resources/CardData/Scripts/GULAG/Bitka o příděly.cs
@@ -14,7 +14,9 @@ public class Bitka_o_příděly : CardScriptBase
         for (int i = 0; i < minions.Count(); i++)
         {
             en.MoveNext();
-            if (en.Current != spared)en.Current.Death();
+            if (en.Current.gameObject == null) continue;//Bandaid solution for the belowmentioned bug
+            if (en.Current != spared)en.Current.Death();//TODO: This will cause bugs since OnDeath effects may kill the minions that will come after this one.
+            //Probably the OnDeaths should be queued up and not forcehappening but thats for later.
         }
     }
 }
